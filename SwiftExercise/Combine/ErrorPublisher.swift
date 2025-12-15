d98ec6ed03d3e00f582de0f1ee022e3f148fee29
Eric <ericzhang1221@163.com>
2025-12-15 18:03:38 +0800

    新增combine练习

--- SwiftExercise/Combine/ErrorPublisher.swift
+++ SwiftExercise/Combine/ErrorPublisher.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import Combine
+
+//错误处理：模拟一个可能抛出错误的 Publisher（比如网络请求失败），实现：
+//错误重试（最多 3 次，每次间隔 1 秒）；
+//错误降级（失败时返回默认值）；
+//捕获并打印错误信息。
+
+class ErrorPublisher {
+    
+}
--- SwiftExercise/ViewController.swift
+++ SwiftExercise/ViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 import Combine
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    var vm: FormAuthViewModel = FormAuthViewModel()
+    var cancellables = Set<AnyCancellable>()
+    
+    var btn: UIButton = UIButton(type: .roundedRect)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,15 +32,65 @@ class ViewController: UIViewController {
 //        viewModel.subscribe()
 //        viewModel.updateName()
 
-        var combineTest = MyCombineTest()
+//        var combineTest = MyCombineTest()
         
-        combineTest.testBaseOperation()
+//        combineTest.testBaseOperation()
         
-        // 保持程序运行
-        RunLoop.main.run()
 
+        ////////////////////// Form Auth ////////////////////
+        let textfieldName: UITextField = UITextField()
+        textfieldName.bounds = CGRect(origin: CGPoint(x: 0,y: 0), size: CGSize(width: 300, height: 100))
+        textfieldName.center = view.center
+//        textfieldName.borderStyle = .roundedRect
+        textfieldName.layer.borderColor = UIColor.blue.cgColor
+        textfieldName.layer.borderWidth = 1
+        view.addSubview(textfieldName)
+        
+        textfieldName.text = vm.name
+        textfieldName.keyboardType = .default
+        textfieldName.delegate = self
+        
+        vm.$name.sink { [weak self] newValue in
+            print("newValue is \(newValue)")
+            if textfieldName.text != newValue {
+                textfieldName.text = newValue
+            }
+            print("vm newValue is \(self?.vm.name)")
+        }
+        .store(in: &cancellables)
+        
+        
+        btn.setTitle("change value", for: UIControl.State.normal)
+        view.addSubview(btn)
+        btn.frame = CGRect(origin: CGPoint(x: 50, y: 50), size: CGSize(width: 100, height: 50))
+        btn.addTarget(self, action: #selector(changeValue), for: .touchUpInside)
     }
 
-
+    @objc func changeValue() {
+        vm.changeNameInModelData()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        vm.name = textField.text ?? "111"
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let text = textField.text {
+            vm.name = text
+        }
+    }
 }
 
+
+class FormAuthViewModel: ObservableObject {
+    @Published var name: String = ""
+    
+    func changeNameInModelData() {
+        name = "aaa"
+    }
+}
